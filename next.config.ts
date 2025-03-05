5cf21be64554a0a77517aef0e8f1143be5b1bc42
Sofyan Egi <sofyanegil@gmail.com>
2025-03-05 16:47:21 +0700

    feat(next.config): add rewrites for proxying video streams from multiple sources

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,26 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/proxy/cimahi/:path*',
+        destination: 'https://smartcity.cimahikota.go.id/video/:path*',
+      },
+      {
+        source: '/proxy/bandung/:path*',
+        destination: 'https://pelindung.bandung.go.id:3443/video/:path*',
+      },
+      {
+        source: '/proxy/kbb/:path*',
+        destination: 'https://cctv.atcs-dishubkbb.id/:path*',
+      },
+      {
+        source: '/proxy/bandungkab/:path*',
+        destination: 'https://cctv.bandungkab.go.id/:path*',
+      },
+    ];
+  },
   async headers() {
     return [
       {
