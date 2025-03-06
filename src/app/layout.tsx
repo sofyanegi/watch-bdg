27bb746793f6a6aa8a6ab14eb8b840dc38240e75
Sofyan Egi <sofyanegil@gmail.com>
2025-03-06 13:02:43 +0700

    feat(analytics): integrate Google Analytics from @next/third-parties for enhanced tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import 'leaflet/dist/leaflet.css';
 import 'react-leaflet-markercluster/styles';
 
 import { Analytics } from '@vercel/analytics/react';
+import { GoogleAnalytics } from '@next/third-parties/google';
+
 import type { Metadata } from 'next';
 
 import SessionProvider from '@/context/SessionProvider';
@@ -25,6 +27,7 @@ export default function RootLayout({
           <SessionProvider>
             {children}
             <Analytics />
+            <GoogleAnalytics gaId="G-H5ZCEMC97Y" />
           </SessionProvider>
         </ThemeProvider>
       </body>
