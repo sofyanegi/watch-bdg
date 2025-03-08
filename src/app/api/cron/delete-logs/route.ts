4b1a61d485c28e8b11349b649ebd688f5d4b170b
Sofyan Egi <sofyanegil@gmail.com>
2025-03-08 21:48:55 +0700

    fix(log): update log deletion to target 'logs_user_access' collection

--- src/app/api/cron/delete-logs/route.ts
+++ src/app/api/cron/delete-logs/route.ts
@@ -3,7 +3,7 @@ import { deleteFilteredDocuments } from '@/services/firebase';
 
 export async function GET() {
   try {
-    await deleteFilteredDocuments('logs', [{ field: 'ttl', op: '<', value: new Date() }]);
+    await deleteFilteredDocuments('logs_user_access', [{ field: 'ttl', op: '<', value: new Date() }]);
     return NextResponse.json({ message: 'Expired logs deleted successfully' });
   } catch (error) {
     return NextResponse.json({ message: 'Error deleting documents', error }, { status: 500 });
